f840c76d46bc0e5c03b925db38c5cae8f7fb9e84
enyeel <anjel.hdz22@gmail.com>
2025-10-27 11:24:47 -0600

    configuré next.js para static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /* Tus opciones de config van aquí */
+
+  // Le dice a Next.js que genere una carpeta 'out' (static export)
+  // cuando corras 'npm run build'.
+  output: 'export',
+
+  // Es MUY probable que también necesites esto.
+  // El componente <Image> de Next.js no funciona bien en un
+  // hosting estático (como Firebase Hosting) sin esto.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
+
+
